9d4e94253379b1b39a08e4467f8f22ef500e0217
SouzaEduardoAC <souzaeduardoac@gmail.com>
2018-08-30 09:47:47 -0300

    Singleton implementation

---
+++ src/main/java/br/com/souzaeduardoac/designpattern/singleton/Run.java
@@ -0,0 +1,21 @@
+package br.com.souzaeduardoac.designpattern.singleton;
+
+public class Run {
+    public static void main(String[] args) {
+        System.out.println(Universe.getInstance());
+        System.out.println(Universe.getInstance());
+        System.out.println(Universe.getInstance());
+        System.out.println(Universe.getInstance());
+        System.out.println(Universe.getInstance());
+        System.out.println(Universe.getInstance());
+        System.out.println(Universe.getInstance());
+        System.out.println(Universe.getInstance());
+        System.out.println(Universe.getInstance());
+        System.out.println(Universe.getInstance());
+        System.out.println(Universe.getInstance());
+        System.out.println(Universe.getInstance());
+        System.out.println(Universe.getInstance());
+        System.out.println(Universe.getInstance());
+        System.out.println(Universe.getInstance());
+    }
+}
